38bcbf0609e07eb96ff5e074df0bb53ee2466cde
erikmura44 <erikmura44@gmail.com>
2016-09-16 10:06:28 -0600

    updated knex js

--- db/knex.js
+++ db/knex.js
@@ -1,5 +1,6 @@
 var environment = process.env.NODE_ENV || 'development';
 console.log('environment in knex.js',environment);
 var config = require('../knexfile.js')[environment];
+var knex = require('knex');
 
 module.exports = require('knex')(config);
